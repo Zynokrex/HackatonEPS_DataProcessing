8c6291d738c0b11c968c1d5c6f7010a13bf41314
Zynokrex <81986384+Zynokrex@users.noreply.github.com>
2023-11-25 23:37:00 +0100

    Updated

--- CreateDF.py
+++ CreateDF.py
@@ -1,32 +1,58 @@
 #%%
 import pandas as pd
-import json
-from itertools import chain
-from collections import ChainMap
+import csv
+import re
+from sklearn.preprocessing import MultiLabelBinarizer
+import matplotlib.pyplot as plt
+#%%
 
-with open('response.json') as f:
-    data_dict = json.load(f)
+descr_list=[]
+
+with open('save_descriptions.csv', mode="r") as csvfile:
+    reader = csv.reader(csvfile, delimiter=";")
+    for entry in reader:
+        descr_list.append(entry)
+csvfile.close()
+
+#%%
+descr_list2=[]
+with open('save_descriptions_2.csv', mode="r") as csvfile2:
+    reader = csv.reader(csvfile2, delimiter=";")
+    for entry in reader:
+        descr_list2.append(entry)
+csvfile2.close()
+
 
 # %%
-data_list = data_dict['response']['comparables']
+descr_dict = {}
+for i in range(len(descr_list[0])):
+    labels = re.findall(r"label':\s*'([^']+)'", descr_list[0][i])
+    score = re.findall(r"'score':\s*([0-9]+(?:\.[0-9]+)?)", descr_list[0][i])
+    if len(labels)==0 or len(score)==0:
+        continue
+    descr_dict[i] = {'label' : labels, 'score':float(score[0])}
 
-
+descr_dict2 = {}
+for i in range(len(descr_list2[0])):
+    labels = re.findall(r"label':\s*'([^']+)'", descr_list2[0][i])
+    score = re.findall(r"'score':\s*([0-9]+(?:\.[0-9]+)?)", descr_list2[0][i])
+    if len(labels)==0 or len(score)==0:
+        continue
+    descr_dict2[i] = {'label' : labels, 'score':float(score[0])}
 # %%
-grouped_dict = {}
-[grouped_dict.setdefault(key, []).append(value) for d in data_list for key, value in d.items()]
-
+labeled_df = pd.DataFrame.from_dict(descr_dict).T
+labeled_df2 = pd.DataFrame.from_dict(descr_dict2).T
+labeled_df = pd.concat([labeled_df, labeled_df2])
 # %%
-ordered_dict = {}
-for key, value in grouped_dict.items():
-    for i, inner_dict in enumerate(value):
-        if key == 'media':
-            continue
-        ordered_dict.setdefault(i, []).append(inner_dict)
+labeled_df['num_labels'] = labeled_df.label.apply(len)
 # %%
-final_dict = {}
-for key, value in ordered_dict.items():
-    final_dict[key] = dict(ChainMap(*value))
+mlb = MultiLabelBinarizer(sparse_output=True)
+labeled_df = labeled_df.join(pd.DataFrame.sparse.from_spmatrix(mlb.fit_transform(labeled_df.pop('label')),
+                                                                index=labeled_df.index,
+                                                                columns=mlb.classes_))
 # %%
-df = pd.DataFrame.from_dict(final_dict).T
+inform_df = pd.DataFrame((labeled_df.sum()/(labeled_df.sum().num_labels)) * 100).T.drop(columns=['score','num_labels'])
 # %%
-
+inform_df.to_csv('pcntg_labels.csv')
+labeled_df.to_csv('Processed_dataset.csv')
+# %%
