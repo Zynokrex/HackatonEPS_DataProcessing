7b13f676ebc555acedd68232eb84fa1053943193
Zynokrex <81986384+Zynokrex@users.noreply.github.com>
2023-11-26 01:32:11 +0100

    pep8 formating

--- CreateDF.py
+++ CreateDF.py
@@ -1,23 +1,21 @@
-#%%
+# %%
 import pandas as pd
 import csv
 import re
 from sklearn.preprocessing import MultiLabelBinarizer
 import matplotlib.pyplot as plt
-#%%
+# %%
 
-descr_list=[]
+descr_list = []
 
-#open first description file and save each string dictionary on the list
 with open('save_descriptions.csv', mode="r") as csvfile:
     reader = csv.reader(csvfile, delimiter=";")
     for entry in reader:
         descr_list.append(entry)
 csvfile.close()
 
-#%%
-#open the second description file and do the same as before
-descr_list2=[]
+# %%
+descr_list2 = []
 with open('save_descriptions_2.csv', mode="r") as csvfile2:
     reader = csv.reader(csvfile2, delimiter=";")
     for entry in reader:
@@ -26,45 +24,36 @@ csvfile2.close()
 
 
 # %%
-#iterate over each description list to obtain the labels and the score using regex expresions
-#as it reads the dictionaries as strings
 descr_dict = {}
 for i in range(len(descr_list[0])):
     labels = re.findall(r"label':\s*'([^']+)'", descr_list[0][i])
     score = re.findall(r"'score':\s*([0-9]+(?:\.[0-9]+)?)", descr_list[0][i])
-    if len(labels)==0 or len(score)==0:
+    if len(labels) == 0 or len(score) == 0:
         continue
-    descr_dict[i] = {'label' : labels, 'score':float(score[0])}
+    descr_dict[i] = {'label': labels, 'score': float(score[0])}
 
 descr_dict2 = {}
 for i in range(len(descr_list2[0])):
     labels = re.findall(r"label':\s*'([^']+)'", descr_list2[0][i])
     score = re.findall(r"'score':\s*([0-9]+(?:\.[0-9]+)?)", descr_list2[0][i])
-    if len(labels)==0 or len(score)==0:
+    if len(labels) == 0 or len(score) == 0:
         continue
-    descr_dict2[i] = {'label' : labels, 'score':float(score[0])}
-    
+    descr_dict2[i] = {'label': labels, 'score': float(score[0])}
 # %%
-#concatenate both dataframes with all the labels and scores
 labeled_df = pd.DataFrame.from_dict(descr_dict).T
 labeled_df2 = pd.DataFrame.from_dict(descr_dict2).T
 labeled_df = pd.concat([labeled_df, labeled_df2])
-
 # %%
-#obtain the number of labels that each row has
 labeled_df['num_labels'] = labeled_df.label.apply(len)
-
 # %%
-#One-hot encoding of the labels
 mlb = MultiLabelBinarizer(sparse_output=True)
 labeled_df = labeled_df.join(pd.DataFrame.sparse.from_spmatrix(mlb.fit_transform(labeled_df.pop('label')),
-                                                                index=labeled_df.index,
-                                                                columns=mlb.classes_))
+                                                               index=labeled_df.index,
+                                                               columns=mlb.classes_))
 # %%
-#Calculate the percentage that each label represents on the dataframe
-inform_df = pd.DataFrame((labeled_df.sum()/(labeled_df.sum().num_labels)) * 100).T.drop(columns=['score','num_labels'])
-
+inform_df = pd.DataFrame((labeled_df.sum(
+)/(labeled_df.sum().num_labels)) * 100).T.drop(columns=['score', 'num_labels'])
 # %%
-#saving the procesed data for its posterior use
 inform_df.to_csv('pcntg_labels.csv')
 labeled_df.to_csv('Processed_dataset.csv')
+# %%
--- DF_conformation.py
+++ DF_conformation.py
@@ -3,18 +3,18 @@ from fastapi import FastAPI
 from fastapi import Request
 import requests
 import pandas as pd
-import csv 
+import csv
 
-#%
-#function for requesting to the API the use of a model
 app = FastAPI()
+
+
 @app.get("/vision")
 def read_vision(img_url):
     url = 'https://api-us.restb.ai/vision/v2/multipredict'
     payload = {
         # Add your client key
         'client_key': 'b1df17d9f440b1a56f07936fb84c56039d4374c86c91618a938e465e8194876a',
-        'model_id': ['re_condition_r1r6_global', 're_features_v5'], 
+        'model_id': ['re_condition_r1r6_global', 're_features_v5'],
         # Add the image URL you want to process
         'image_url': img_url
     }
@@ -26,21 +26,19 @@ def read_vision(img_url):
     json_response = response.json()
     return json_response
 
-#%%
-#Reading the extracted urls of images
-df = pd.read_csv('urls.csv', index_col=0, sep = ';').reset_index()
+
+# %%
+df = pd.read_csv('urls.csv', index_col=0, sep=';').reset_index()
 
 
 # %%
-#getting a random sample of the df to avoid surpasing the request limit
 description_df = df.sample(n=3600, random_state=434143)
-
 # %%
-#saving per batches the requests sent to the api
 with open('save_descriptions.csv', 'w', newline='') as file:
     for idx in description_df.index:
         descr = read_vision(description_df['Url'][idx])
         file.write(str(descr) + ";")
-        if idx%10 == 0:
+        if idx % 10 == 0:
             file.flush()
 file.close()
+# %%
